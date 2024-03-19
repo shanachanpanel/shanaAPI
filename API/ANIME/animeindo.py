c2b0fde7eebdf811bb6a9f2ba3f8ec14e15f6993
shanachan xixixi <126304667+shanachanpanel@users.noreply.github.com>
2024-03-19 18:45:54 +0700

    Update animeindo.py

--- API/ANIME/animeindo.py
+++ API/ANIME/animeindo.py
@@ -1,97 +1,347 @@
-import typing
-import datetime
-import json
-import requests
+const url = require("../helpers/base-url");
+const { default: Axios } = require("axios");
+const cheerio = require("cheerio");
+const errors = require("../helpers/errors");
+const episodeHelper = require("../helpers/episodeHelper");
+const {baseUrl} = require("../helpers/base-url");
+const e = require("express");
 
-from anime_api import exceptions
-from anime_api.apis.nekos_moe.objects import User, Image, PendingImage
-from anime_api.apis.nekos_moe.types import SearchSort
+exports.detailAnime = async (req, res) => {
+  const id = req.params.id;
+  const fullUrl = url.baseUrl + `anime/${id}`;
+  // console.log(fullUrl);
+  try {
+    const response = await Axios.get(fullUrl);
 
+    const $ = cheerio.load(response.data);
+    const detailElement = $(".venser").find(".fotoanime");
+    const epsElement = $("#_epslist").html();
+    let object = {};
+    let episode_list = [];
+    object.thumb = detailElement.find("img").attr("src");
+    object.anime_id = req.params.id;
+    let genre_name, genre_id, genre_link;
+    let genreList = [];
 
-class NekosMoeAPI:
-    """
-    Docs: https://docs.nekos.moe/
-    """
+    object.synopsis = $("#venkonten > div.venser > div.fotoanime > div.sinopc")
+      .find("p")
+      .text();
 
-    endpoint = "https://nekos.moe/api/v1"
-    api_token = None
+    detailElement.find(".infozin").filter(function () {
+      object.title = $(this)
+        .find("p")
+        .children()
+        .eq(0)
+        .text()
+        .replace("Judul: ", "");
+      object.japanase = $(this)
+        .find("p")
+        .children()
+        .eq(1)
+        .text()
+        .replace("Japanese: ", "");
+      object.score = parseFloat(
+        $(this).find("p").children().eq(2).text().replace("Skor: ", "")
+      );
+      object.producer = $(this)
+        .find("p")
+        .children()
+        .eq(3)
+        .text()
+        .replace("Produser:  ", "");
+      object.type = $(this)
+        .find("p")
+        .children()
+        .eq(4)
+        .text()
+        .replace("Tipe: ", "");
+      object.status = $(this)
+        .find("p")
+        .children()
+        .eq(5)
+        .text()
+        .replace("Status: ", "");
+      object.total_episode = parseInt(
+        $(this).find("p").children().eq(6).text().replace("Total Episode: ", "")
+      );
+      object.duration = $(this)
+        .find("p")
+        .children()
+        .eq(7)
+        .text()
+        .replace("Durasi: ", "");
+      object.release_date = $(this)
+        .find("p")
+        .children()
+        .eq(8)
+        .text()
+        .replace("Tanggal Rilis: ", "");
+      object.studio = $(this)
+        .find("p")
+        .children()
+        .eq(9)
+        .text()
+        .replace("Studio: ", "");
+      $(this)
+        .find("p")
+        .children()
+        .eq(10)
+        .find("span > a")
+        .each(function () {
+          genre_name = $(this).text();
+          genre_id = $(this)
+            .attr("href")
+            .replace(`https://otakudesu.moe/genres/`, "");
+          genre_link = $(this).attr("href");
+          genreList.push({ genre_name, genre_id, genre_link });
+          object.genre_list = genreList;
+        });
+    });
 
-    def __init__(
-        self,
-        username: typing.Optional[str] = None,
-        password: typing.Optional[str] = None,
-        api_token: typing.Optional[str] = None,
-    ):
-        if api_token:
-            self.api_token = api_token
-        elif username and password:
-            self.api_token = self.get_api_token(username, password)
+    $("#venkonten > div.venser > div:nth-child(8) > ul > li").each(
+      (i, element) => {
+        const dataList = {
+          title: $(element).find("span > a").text(),
+          id: $(element)
+            .find("span > a")
+            .attr("href")
+            .replace('https://otakudesu.moe/', ""),
+          link: $(element).find("span > a").attr("href"),
+          uploaded_on: $(element).find(".zeebr").text(),
+        };
+        episode_list.push(dataList);
+      }
+    );
+    object.episode_list =
+      episode_list.length === 0
+        ? [
+            {
+              title: "Masih kosong gan",
+              id: "Masih kosong gan",
+              link: "Masih kosong gan",
+              uploaded_on: "Masih kosong gan",
+            },
+          ]
+        : episode_list;
+    const batch_link = {
+      id:
+        $("div.venser > div:nth-child(6) > ul").text().length !== 0
+          ? $("div.venser > div:nth-child(6) > ul > li > span:nth-child(1) > a")
+              .attr("href")
+              .replace(`https://otakudesu.moe/batch/`, "")
+          : "Masih kosong gan",
+      link:
+        $("div.venser > div:nth-child(6) > ul").text().length !== 0
+          ? $(
+              "div.venser > div:nth-child(6) > ul > li > span:nth-child(1) > a"
+            ).attr("href")
+          : "Masih kosong gan",
+    };
+    const empty_link = {
+      id: "Masih kosong gan",
+      link: "Masih kosong gan",
+    };
+    object.batch_link = batch_link;
+    //console.log(epsElement);
+    res.json(object);
+  } catch (err) {
+    console.log(err);
+    errors.requestFailed(req, res, err);
+  }
+};
+exports.batchAnime = async (req, res) => {
+  const id = req.params.id;
+  const fullUrl = `${baseUrl}batch/${id}`;
+  console.log(fullUrl);
+  Axios.get(fullUrl)
+    .then((response) => {
+      const $ = cheerio.load(response.data);
+      const obj = {};
+      obj.title = $(".batchlink > h4").text();
+      obj.status = "success";
+      obj.baseUrl = fullUrl;
+      let low_quality = _batchQualityFunction(0, response.data);
+      let medium_quality = _batchQualityFunction(1, response.data);
+      let high_quality = _batchQualityFunction(2, response.data);
+      obj.download_list = { low_quality, medium_quality, high_quality };
+      res.send(obj);
+    })
+    .catch((err) => {
+      errors.requestFailed(req, res, err);
+    });
+};
+exports.epsAnime = async (req, res) => {
+  const id = req.params.id;
+  const fullUrl = `${url.baseUrl}${id}`;
+  try {
+    const response = await Axios.get(fullUrl);
+    const $ = cheerio.load(response.data);
+    const streamElement = $("#lightsVideo").find("#embed_holder");
+    const obj = {};
+    obj.title = $(".venutama > h1").text();
+    obj.baseUrl = fullUrl;
+    obj.id = fullUrl.replace(url.baseUrl, "");
+    const streamLink = streamElement.find("iframe").attr("src");
+    // const streamLinkResponse = await Axios.get(streamLink)
+    // const stream$ = cheerio.load(streamLinkResponse.data)
+    // const sl = stream$('body').find('script').html().search('sources')
+    // const endIndex = stream$('body').find('script').eq(0).html().indexOf('}]',sl)
+    // const val = stream$('body').find('script').eq(0).html().substr(sl,endIndex - sl+1).replace(`sources: [{'file':'`,'')
+    // console.log(val);
+    // console.log(val.replace(`','type':'video/mp4'}`,''));
+    obj.link_stream = await episodeHelper.get(streamLink);
+    console.log($('#pembed > div > iframe').attr('src'));
+    let low_quality;
+    let medium_quality;
+    let high_quality;
+    let mirror1 = [];
+    let mirror2 = [];
+    let mirror3 = [];
 
-    def get_api_token(self, username: str, password: str) -> str:
-        """
-        Get an API token from username and password.
-        """
+    $('#embed_holder > div.mirrorstream > ul.m360p > li').each((idx,el)=>{``
+      mirror1.push({
+        host:$(el).find('a').text().trim(),
+        id:$(el).find('a').attr('href'),
+      });
+    });
+    $('#embed_holder > div.mirrorstream > ul.m480p > li').each((idx,el)=>{
+      mirror2.push({
+        host:$(el).find('a').text().trim(),
+        id:$(el).find('a').attr('href'),
+      });
+    });
+    $('#embed_holder > div.mirrorstream > ul.m720p > li').each((idx,el)=>{
+      mirror3.push({
+        host:$(el).find('a').text().trim(),
+        id:$(el).find('a').attr('href'),
+      });
+    });
+    obj.mirror1 = {quality:'360p',mirrorList:mirror1}
+    obj.mirror2 = {quality:'480p',mirrorList:mirror2}
+    obj.mirror3 = {quality:'720p',mirrorList:mirror3}
+    if($('#venkonten > div.venser > div.venutama > div.download > ul > li:nth-child(1)').text() === ''){
+      console.log('ul is empty');
+      low_quality = _notFoundQualityHandler(response.data,0)
+      medium_quality = _notFoundQualityHandler(response.data,1)
+      high_quality = _notFoundQualityHandler(response.data,2)
+    }else{
+      console.log('ul is not empty');
+      low_quality = _epsQualityFunction(0, response.data);
+      medium_quality = _epsQualityFunction(1, response.data);
+      high_quality = _epsQualityFunction(2, response.data);
+    }
+    obj.quality = { low_quality, medium_quality, high_quality };
+    res.send(obj);
+  } catch (err) {
+    console.log(err);
+    errors.requestFailed(req, res, err);
+  }
+};
 
-        response = requests.post(
-            f"{self.endpoint}/auth",
-            json={"username": username, "password": password},
-        )
+exports.epsMirror = async (req, res) => {
+  const mirrorId = req.body.mirrorId;
+  const animeId = req.params.animeId;
+  const fullUrl = `${baseUrl}${animeId}/${mirrorId}`;
+  try {
+    const response = await Axios.get(fullUrl);
+    const $ = cheerio.load(response.data);
+    const obj = {};
+    obj.title = $(".venutama > h1").text();
+    obj.baseUrl = fullUrl;
+    obj.id = fullUrl.replace(url.baseUrl, "");
+    const streamLink = $('#pembed > div > iframe').attr('src')
+    obj.streamLink = streamLink
+    obj.link_stream = await episodeHelper.get(streamLink);
+    res.send(obj);
+  } catch (error) {
+    console.log(error);
+    errors.requestFailed(req, res, err);
+  }
+}
 
-        if response.status_code != 200:
-            raise exceptions.ServerError(
-                status_code=response.status_code,
-                msg=response.json().get("message", "Unknown error"),
-            )
+function _batchQualityFunction(num, res) {
+  const $ = cheerio.load(res);
+  const element = $(".download").find(".batchlink");
+  const download_links = [];
+  let response;
+  element.find("ul").filter(function () {
+    const quality = $(this).find("li").eq(num).find("strong").text();
+    const size = $(this).find("li").eq(num).find("i").text();
+    $(this)
+      .find("li")
+      .eq(num)
+      .find("a")
+      .each(function () {
+        const _list = {
+          host: $(this).text(),
+          link: $(this).attr("href"),
+        };
+        download_links.push(_list);
+        response = { quality, size, download_links };
+      });
+  });
+  return response;
+}
+function _epsQualityFunction(num, res) {
+  const $ = cheerio.load(res);
+  const element = $(".download");
+  const download_links = [];
+  let response;
 
-        return response.json()["token"]
+  element.find("ul").filter(function () {
+    const quality = $(this).find("li").eq(num).find("strong").text();
+    const size = $(this).find("li").eq(num).find("i").text();
+    $(this).find("li").eq(num).find("a").each(function () {
+        const _list = {
+          host: $(this).text(),
+          link: $(this).attr("href"),
+        };
+        download_links.push(_list);
+        response = { quality, size, download_links };
+        
+      });
+  });
+  return response;
+}
 
-    def regenerate_api_token(
-        self,
-        username: typing.Optional[str] = None,
-        password: typing.Optional[str] = None,
-    ) -> bool:
-        """
-        Regenerate the API token.
-        """
+function _notFoundQualityHandler(res,num){
+  const $ = cheerio.load(res);
+  const download_links = [];
+  const element = $('.download')
+  let response;
 
-        response = requests.post(
-            f"{self.endpoint}/auth/regen",
-            headers={"Authorization": self.api_token} if self.api_token else None,
-        )
+  element.filter(function(){
+    if($(this).find('.anime-box > .anime-title').eq(0).text() === ''){
+      $(this).find('.yondarkness-box').filter(function(){
+        const quality = $(this).find('.yondarkness-title').eq(num).text().split('[')[1].split(']')[0];
+        const size = $(this).find('.yondarkness-title').eq(num).text().split(']')[1].split('[')[1];
+        $(this).find('.yondarkness-item').eq(num).find('a').each((idx,el) => {
+          const _list = {
+            host: $(el).text(),
+            link: $(el).attr("href"),
+          };
+          download_links.push(_list);
+          response = { quality, size, download_links };
+        })
+      })
+    }else{
+      $(this).find('.anime-box').filter(function(){
+        const quality = $(this).find('.anime-title').eq(num).text().split('[')[1].split(']')[0];
+        const size = $(this).find('.anime-title').eq(num).text().split(']')[1].split('[')[1];
+        $(this).find('.anime-item').eq(num).find('a').each((idx,el) => {
+          const _list = {
+            host: $(el).text(),
+            link: $(el).attr("href"),
+          };
+          download_links.push(_list);
+          response = { quality, size, download_links };
+        })
+      })
+    }
+  })
+  return response;
 
-        if response.status_code != 204:
-            raise exceptions.ServerError(
-                status_code=response.status_code,
-                msg=response.json().get("message", "Unknown error"),
-            )
-
-        if username and password:
-            self.api_token = self.get_api_token(username, password)
-        elif username or password:
-            raise ValueError("You must provide both a username and password.")
-        elif username or password:
-            raise ValueError("You must provide a username and password. You provided only one.")
-
-        return True
-
-    def get_user(self, user_id: str = "@me") -> User:
-        """
-        Get information about a user.
-        """
-
-        response = requests.get(
-            f"{self.endpoint}/user/{user_id}",
-            headers={"Authorization": self.api_token} if self.api_token else None,
-        )
-
-        if response.status_code != 200:
-            raise exceptions.ServerError(
-                status_code=response.status_code,
-                msg=response.json().get("message", "Unknown error"),
-            )
-
-        user = response.json()["user"]
-
+}
         return User(
             _api=self,
             _id=user["id"],
